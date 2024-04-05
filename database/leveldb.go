7f0fe45554a9b3059b64d94f5f6e56b968ce041a
Lazar Travica <lazar.travica@route3.co>
2024-04-05 10:58:08 +0200

    Added more fine-tuning params for LevelDB

--- database/leveldb.go
+++ database/leveldb.go
@@ -4,10 +4,77 @@ import (
 	"fmt"
 	"github.com/armon/go-metrics"
 	"github.com/syndtr/goleveldb/leveldb"
+	"github.com/syndtr/goleveldb/leveldb/filter"
+	"github.com/syndtr/goleveldb/leveldb/opt"
+	"os"
+	"strconv"
+	"syscall"
 	"time"
 )
 
+const (
+	bloomFilter              = "LEVELDB_BLOOM_FILTER"
+	disableSeekCompaction    = "LEVELDB_DISABLE_SEEK_COMPACTION"
+	cacheSizeMb              = "LEVELDB_CACHE_SIZE_MB"
+	forceFileDescriptorLimit = "LEVELDB_FORCE_FILE_DESCRIPTOR_LIMIT"
+)
+
+func isEnvSet(name string) bool {
+	return os.Getenv(name) != ""
+}
+
+func getEnvInt(name string) (int, error) {
+	return strconv.Atoi(os.Getenv(name))
+}
+
+func raiseAndReturnFDLimit() (uint64, error) {
+	var limit syscall.Rlimit
+
+	if err := syscall.Getrlimit(syscall.RLIMIT_NOFILE, &limit); err != nil {
+		return 0, err
+	}
+
+	if err := syscall.Setrlimit(syscall.RLIMIT_NOFILE, &limit); err != nil {
+		return 0, err
+	}
+	// MacOS can silently apply further caps, so retrieve the actually set limit
+	if err := syscall.Getrlimit(syscall.RLIMIT_NOFILE, &limit); err != nil {
+		return 0, err
+	}
+	return limit.Cur, nil
+}
+
 func NewLevelDB(path string, name string) (db *leveldb.DB, err error) {
+	opts := &opt.Options{}
+
+	if isEnvSet(forceFileDescriptorLimit) {
+		limit, err := raiseAndReturnFDLimit()
+
+		if err != nil {
+			return nil, err
+		}
+
+		opts.OpenFilesCacheCapacity = int(limit / 2)
+
+		fmt.Println("[[[[LEVELDB]]]] Forcing file descriptors to half of max")
+	}
+
+	if isEnvSet(bloomFilter) {
+		opts.Filter = filter.NewBloomFilter(10)
+		fmt.Println("[[[[LEVELDB]]]] Initialized 10 bit bloom filter")
+	}
+
+	if isEnvSet(disableSeekCompaction) {
+		opts.DisableSeeksCompaction = true
+		fmt.Println("[[[[LEVELDB]]]] Disabled seek compaction")
+	}
+
+	if value, err := getEnvInt(cacheSizeMb); err != nil {
+		opts.BlockCacheCapacity = value / 2 * opt.MiB
+		opts.WriteBuffer = value / 4 * opt.MiB
+		fmt.Println("[[[[LEVELDB]]]] Set cache size")
+	}
+
 	db, err = leveldb.OpenFile(path, nil)
 	if err != nil {
 		return
